7c444d11d4a478e6255524f63cce1967ab88d0d2
zaruhidanielyan <97274535+zaruhidanielyan@users.noreply.github.com>
2026-01-31 12:47:49 +0000

    Updated call_llm

--- moneychanger.py
+++ moneychanger.py
@@ -4,6 +4,17 @@ import os
 import requests
 from dotenv import load_dotenv
 import json
+import streamlit as st
+from openai import OpenAI
+
+token = os.environ["GITHUB_TOKEN"]
+endpoint = "https://models.github.ai/inference"
+model_name = "openai/gpt-4o"
+
+client = OpenAI(
+    base_url=endpoint,
+    api_key=token,
+)
 
 load_dotenv()
 EXCHANGERATE_API_KEY = os.getenv('EXCHANGERATE_API_KEY')
@@ -14,17 +25,33 @@ def get_exchange_rate(base: str, target: str, amount: str) -> Tuple:
     response = json.loads(requests.get(url).text)
     return (base, target, amount, f'{response["conversion_result"]:.2f}')
 
-print(get_exchange_rate("USD", "AMD", "240"))
+# print(get_exchange_rate("USD", "AMD", "240"))
 
 def call_llm(textbox_input) -> Dict:
     """Make a call to the LLM with the textbox_input as the prompt.
        The output from the LLM should be a JSON (dict) with the base, amount and target"""
     try:
-        completion = ...
+        response = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "system",
+                    "content": "You are a helpful assistant.",
+                },
+                {
+                    "role": "user",
+                    "content": textbox_input,
+                }
+            ],
+            temperature=1.0,
+            top_p=1.0,
+            max_tokens=1000,
+            model=model_name
+        )
+
     except Exception as e:
         print(f"Exception {e} for {text}")
     else:
-        return completion
+        return response.choices[0].message.content
 
 def run_pipeline():
     """Based on textbox_input, determine if you need to use the tools (function calling) for the LLM.
@@ -39,3 +66,8 @@ def run_pipeline():
         st.write(f"(Function calling not used) and response from the model")
     else:
         st.write("NotImplemented")
+
+st.title("Multilingual Money Changer")
+user_input = st.text_input("Enter the amount an the currency")
+if st.button("Submit"):
+    st.write(call_llm(user_input))
